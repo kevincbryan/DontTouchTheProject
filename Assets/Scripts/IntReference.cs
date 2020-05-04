f5ebe2eb1ffeffb4ec14af05059aced8b6abce14
kevincbryan <kevincbryan@gmail.com>
2020-05-04 10:50:42 -0700

    Added Scriptable Objects and Editor Tweaks
    Added IntVariable and FloatVariable and IntReference and FloatReference to contain them

---
+++ Assets/Scripts/IntReference.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public class IntReference
+{
+
+    public bool UseConstant = true;
+    public int ConstantValue;
+    public IntVariable Variable;
+
+    public int Value
+    {
+        get { return UseConstant ? ConstantValue : Variable.Value; }
+        set
+        {
+            if (UseConstant)
+            {
+                ConstantValue = value;
+            }
+            else
+            {
+                Variable.Value = value;
+            }
+        }
+    }
+}
