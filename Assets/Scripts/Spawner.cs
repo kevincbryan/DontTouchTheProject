075b57b5db5914f04d2ba6658d5b84a55558754e
kevincbryan <kevincbryan@gmail.com>
2020-05-04 15:02:49 -0700

    Added Enemy Prefabs

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Enemy { child, parent, zookeeper };
+
+public class Spawner : MonoBehaviour
+{
+    public Transform[] spawnLoc;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
