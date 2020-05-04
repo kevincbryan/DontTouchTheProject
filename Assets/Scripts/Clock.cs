8f3e17bc1a614acf99252e62aa6b41f8e707598a
kevincbryan <kevincbryan@gmail.com>
2020-05-04 14:16:10 -0700

    Added Clock script and Scriptable Objects

---
+++ Assets/Scripts/Clock.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clock : MonoBehaviour
+{
+    public FloatVariable myTime;
+    public FloatVariable maxTime;
+    public bool gameEnd = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myTime.Value = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myTime.Value += Time.deltaTime;
+
+        if (myTime.Value > maxTime.Value)
+        {
+            gameEnd = true;
+        }
+
+    }
+}
